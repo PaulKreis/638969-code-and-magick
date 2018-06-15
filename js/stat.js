fc7e53f79c368e787af3d68064845da2b0b891f0
Pavel Kreys <kreislab@gmail.com>
2018-06-15 21:48:30 +0300

    Правки по комментариям №4

--- js/stat.js
+++ js/stat.js
@@ -12,15 +12,16 @@ var BAR_HEIGHT = 150;
 var BAR_WIDTH = 40;
 var INTRO_TEXT_X = 110;
 var INTRO_TEXT_Y = 30;
+var INTRO_TEXT_LINE_HEIGHT = 20;
 var DEFAULT_FONT = '16px PT Mono';
 var FONT_BASELINE = 'hanging';
 var FONT_FILL_STYLE = 'black';
 var PLAYER_NAME = 'Вы';
-var PLAYER_NAME_COLOR = 'rgba(255, 0, 0, 1)';
+var PLAYER_COLUMN_COLOR = 'rgba(255, 0, 0, 1)';
 
 
 //  Функция отрисовки цветного текста на координатах
-var renderText = function (ctx, text, x, y, color) {
+var renderText = function (ctx, x, y, text, color) {
   ctx.fillStyle = color;
   ctx.fillText(text, x, y);
 };
@@ -32,7 +33,7 @@ var renderCloud = function (ctx, x, y, color) {
 };
 
 //  Функция получения случайного оттенка синего
-var getRndColor = function () {
+var getRandomBlueColor = function () {
   return 'rgba(0, 0, 255, ' + Math.random().toFixed(2) + ')';
 };
 
@@ -53,29 +54,37 @@ var renderColumn = function (ctx, x, y, width, height, color) {
   ctx.fillRect(x, y, width, height);
 };
 
+//  Функция отрисовки вступительного текста
+var renderTitle = function (ctx) {
+  ctx.font = DEFAULT_FONT;
+  ctx.textBaseline = FONT_BASELINE;
+  renderText(ctx, INTRO_TEXT_X, INTRO_TEXT_Y, 'Ура вы победили!', FONT_FILL_STYLE);
+  renderText(ctx, INTRO_TEXT_X, INTRO_TEXT_Y + INTRO_TEXT_LINE_HEIGHT, 'Список результатов:', FONT_FILL_STYLE);
+};
+
 window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_X + 10, CLOUD_Y + 10, 'rgba(0, 0, 0, 0.3)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
-  ctx.font = DEFAULT_FONT;
-  ctx.textBaseline = FONT_BASELINE;
-  renderText(ctx, 'Ура вы победили!', INTRO_TEXT_X, INTRO_TEXT_Y, FONT_FILL_STYLE);
-  renderText(ctx, 'Список результатов:', INTRO_TEXT_X, INTRO_TEXT_Y + 20, FONT_FILL_STYLE);
+  renderTitle(ctx);
 
   var maxTime = getMaxElement(times);
-  var leftGap = (CLOUD_WIDTH - (BAR_WIDTH * names.length + COLUMN_GAP * (names.length - 1))) / 2;
+  var leftColumnsGap = (CLOUD_WIDTH - (BAR_WIDTH * names.length + COLUMN_GAP * (names.length - 1))) / 2;
 
   //  Отрисовка столбиков и текста
   for (var i = 0; i < names.length; i++) {
-    var columnX = CLOUD_X + leftGap + (BAR_WIDTH + COLUMN_GAP) * i;
+    var columnX = CLOUD_X + leftColumnsGap + (BAR_WIDTH + COLUMN_GAP) * i;
+
+    renderText(ctx, columnX, CLOUD_Y + CLOUD_HEIGHT - NAMES_FONT_GAP, names[i], FONT_FILL_STYLE);
+    renderText(ctx, columnX, CLOUD_HEIGHT - ((BAR_HEIGHT * times[i]) / maxTime) - TIME_FONT_GAP, Math.round(times[i]), FONT_FILL_STYLE);
+
+    var barHeight = -1 * (BAR_HEIGHT * times[i]) / maxTime;
+    var columnColor = getRandomBlueColor();
 
     if (names[i] === PLAYER_NAME) {
-      var textColor = PLAYER_NAME_COLOR;
-    } else {
-      textColor = FONT_FILL_STYLE;
+      columnColor = PLAYER_COLUMN_COLOR;
     }
-    renderText(ctx, names[i], columnX, CLOUD_Y + CLOUD_HEIGHT - NAMES_FONT_GAP, textColor);
-    renderText(ctx, Math.round(times[i]), columnX, CLOUD_HEIGHT - ((BAR_HEIGHT * times[i]) / maxTime) - TIME_FONT_GAP, FONT_FILL_STYLE);
-    renderColumn(ctx, columnX, COLUMN_Y, BAR_WIDTH, -1 * (BAR_HEIGHT * times[i]) / maxTime, getRndColor());
+
+    renderColumn(ctx, columnX, COLUMN_Y, BAR_WIDTH, barHeight, columnColor);
   }
 };
